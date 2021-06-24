ad0ee2453fe9711be5a0718ebbfb227ea4b62c24
sultan <malsagov.sa@inbox.ru>
2021-06-24 13:16:05 +0300

    3zadremove

---
+++ remove-elements.js
@@ -0,0 +1,17 @@
+let characters = ["Тирион Ланнистер", "Станис Баратеон", "Теон Грейджой", "Эддард Старк", "Дайенерис Таргариен", "Джон Сноу"];
+
+let firstElement = characters[0];
+console.log(firstElement);
+let lastElement = characters[characters.length - 1];
+console.log(lastElement);
+characters.shift();
+console.log(characters);
+characters.push(firstElement);
+console.log(characters);
+characters.unshift("Король Ночи");
+console.log(characters);
+delete characters[5];
+console.log(characters);
+characters.unshift(lastElement);
+
+console.log(characters);
